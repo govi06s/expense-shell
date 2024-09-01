ec552e17b5a96e197a3ef9a193427111248bf0ad
govi06s <dukkagovinda8179@gmail.com>
2024-09-01 18:43:32 +0530

    loops

--- backend.sh
+++ backend.sh
@@ -61,6 +61,6 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading the backend code"
 
 cd /app
-rm-rf /app/*
+rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extarct the backend code"
