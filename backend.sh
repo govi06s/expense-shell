b21d85975f4c2468fbca647451b6ea143cbe3045
govi06s <dukkagovinda8179@gmail.com>
2024-09-01 17:56:23 +0530

    loops

---
+++ backend.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log
+mkdir -p $LOGS_FOLDER
+
+
+USERID=$(id -u)
+
+#echo "UserID is: $USERID"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e " $R please run the script with root prevelages $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is.. $R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is.. $G SUCCESS $N" |tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started excuting: $(date)" | tee -a $LOG_FILE
+CHECK_ROOT
+
+dnf module disable nodejs -y
+VALIDATE $? "Disable the Nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enable the Nodejs:20"
+
+dnf install nodejs -y
+VALIDATE $? "Install the Nodejs"
+
+useradd expense
+VALIDATE $? "Creating user Expense"
