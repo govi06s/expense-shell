8a47b78575943c747bec7a95a3bff789f256c9ee
govi06s <dukkagovinda8179@gmail.com>
2024-09-03 01:07:02 +0530

    loops

--- frontend.sh
+++ frontend.sh
@@ -55,5 +55,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract the application code"
 
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restart the Nginx"
