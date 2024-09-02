334913c3d67cbdf3221c5a6f820bed17652c0d74
govi06s <dukkagovinda8179@gmail.com>
2024-09-03 01:34:47 +0530

    loops

--- frontend.sh
+++ frontend.sh
@@ -39,24 +39,24 @@ CHECK_ROOT
 dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx.."
 
-systemctl enable nginx &>>$LOG_FILE
-VALIDATE $? "Enable the Nginx"
+# systemctl enable nginx &>>$LOG_FILE
+# VALIDATE $? "Enable the Nginx"
 
-#systemctl start nginx
-#VALIDATE $? "Starts the Nginx"
+# systemctl start nginx
+# VALIDATE $? "Starts the Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-VALIDATE $? "Remove the unwanted file"
+# rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+# VALIDATE $? "Remove the unwanted file"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
-VALIDATE $? "Download the frontend application code"
+# curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+# VALIDATE $? "Download the frontend application code"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "Extract the application code"
+# cd /usr/share/nginx/html
+# unzip /tmp/frontend.zip &>>$LOG_FILE
+# VALIDATE $? "Extract the application code"
 
-#cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf 
-#VALIDATE "Copied Expense Nginx"
+# cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf 
+# VALIDATE "Copied Expense Nginx"
 
-systemctl restart nginx #&>>$LOG_FILE
-VALIDATE $? "Restart the Nginx"
+# systemctl restart nginx #&>>$LOG_FILE
+# VALIDATE $? "Restart the Nginx"
